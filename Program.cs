bccd308ff16ab711c068ab356b850d27cb672d57
marco-soria <marco.soriaa7@gmail.com>
2024-11-04 15:38:04 -0500

    added env config

--- Program.cs
+++ Program.cs
@@ -1,15 +1,34 @@
 using efproject;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using DotNetEnv; // Importa DotNetEnv. se debe instalar un paquete de NuGet con dotnet add package DotNetEnv
 
 var builder = WebApplication.CreateBuilder(args);
 
-//builder.Services.AddDbContext<TasksContext>(p => p.UseInMemoryDatabase("TasksDB"));  
-//builder.Services.AddSqlServer<TasksContext>("Data Source=DESKTOP-OJPGDF3;Initial Catalog=TasksDb;Trusted_Connection=True; Integrated Security=True;user id=sa;password=YourStrong!Passw0rd;TrustServerCertificate=True");
+// Cargar el archivo .env
+Env.Load();
 
-//builder.Services.AddSqlServer<TasksContext>("Server=sqlserver;Database=TasksDB;User=sa;Password=YourStrong!Passw0rd;TrustServerCertificate=True; Integrated Security=True");
 
-builder.Services.AddSqlServer<TasksContext>(builder.Configuration.GetConnectionString("SQLServerConnection"));
+//revealing connection
+//builder.Services.AddSqlServer<TasksContext>(builder.Configuration.GetConnectionString("SQLServerConnection"));
+
+
+// Obtener la cadena de conexión
+var connectionString = Environment.GetEnvironmentVariable("SQLSERVER_CONNECTION_STRING");
+
+// Imprimir la cadena de conexión para verificación
+Console.WriteLine($"Connection String from .env: {connectionString}");
+
+// Validar que la cadena de conexión no esté vacía
+if (string.IsNullOrEmpty(connectionString))
+{
+    throw new InvalidOperationException("The connection string was not found in the environment variables.");
+}
+
+// Configurar el contexto de base de datos para usar SQL Server con la cadena de conexión
+builder.Services.AddDbContext<TasksContext>(options =>
+    options.UseSqlServer(connectionString));
+
 
 var app = builder.Build();
 
